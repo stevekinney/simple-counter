f847b88efa5acaa238208a6758ccb13961515ebb
Steve Kinney <hello@stevekinney.net>
2019-05-28 18:33:33 -0500

    Add use-local-storage

--- src/index.js
+++ src/index.js
@@ -3,42 +3,41 @@ import { render } from 'react-dom';
 
 import './styles.scss';
 
-const CounterContext = createContext();
+const counterReducer = (state, action) => {
+  if (action.type === 'INCREMENT') {
+    return { count: state.count + 100 };
+  }
+  return state;
+};
 
-const CounterProvider = ({ children }) => {
-  const [count, setCount] = useState(0);
+const useLocalStorage = (key, defaultValue) => {
+  let [value, setValue] = useState(() => {
+    let storedValue = localStorage.getItem(key);
+    if (storedValue) return JSON.parse(storedValue);
+    return defaultValue;
+  });
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(0);
+  const setValueAndStore = newValue => {
+    localStorage.setItem(key, JSON.stringify(newValue));
+    setValue(newValue);
+  };
 
-  return (
-    <CounterContext.Provider value={{ increment, decrement, reset, count }}>
-      {children}
-    </CounterContext.Provider>
-  );
+  return [value, setValueAndStore];
 };
 
 const Counter = () => {
-  const { count, increment, decrement, reset } = React.useContext(
-    CounterContext,
-  );
+  const [count, setCount] = useLocalStorage('very-important-count');
+
+  const increment = () => setCount(count + 1);
 
   return (
     <main className="Counter">
-      <p className="count">{count}</p>
+      <p className="count">{+count}</p>
       <section className="controls">
         <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-        <button onClick={reset}>Reset</button>
       </section>
     </main>
   );
 };
 
-render(
-  <CounterProvider>
-    <Counter />
-  </CounterProvider>,
-  document.getElementById('root'),
-);
+render(<Counter />, document.getElementById('root'));
