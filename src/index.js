514ed836d7d1d12841c24569b902be643f02bf44
Steve Kinney <hello@stevekinney.net>
2019-05-28 15:33:30 -0500

    Before the Context API

--- src/index.js
+++ src/index.js
@@ -3,19 +3,43 @@ import { render } from 'react-dom';
 
 import './styles.scss';
 
+const inc = ({ count }) => ({ count: count + 1 });
+const dec = ({ count }) => ({ count: count - 1 });
+const res = () => ({ count: 0 });
+
 class Counter extends Component {
+  state = { count: parseInt(localStorage.getItem('count'), 10) || 0 };
+
+  increment = () => {
+    this.setState(this.props.onIncrement, () =>
+      localStorage.setItem('count', this.state.count),
+    );
+  };
+
+  decrement = () => {
+    this.setState(dec);
+  };
+
+  reset = () => {
+    this.setState(res);
+  };
+
   render() {
+    const { count } = this.state;
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </main>
     );
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+render(
+  <Counter onIncrement={state => ({ count: state.count + 100 })} />,
+  document.getElementById('root'),
+);
