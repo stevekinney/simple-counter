e7f19082a781ee5a8b8c38fe83b1a99130d2f45f
Steve Kinney <hello@stevekinney.net>
2019-11-16 15:05:35 -0800

    Get the basic component state implemented

--- src/index.js
+++ src/index.js
@@ -4,14 +4,39 @@ import { render } from 'react-dom';
 import './styles.scss';
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 3,
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  reset() {
+    this.setState({ count: 0 });
+  }
+
   render() {
+    const { count } = this.state;
+
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </main>
     );
